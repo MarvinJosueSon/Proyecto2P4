938f3078eda7d5ef717679c20925e5a6e69b4b17
Marvin <sonmarvint2001@gmail.com>
2025-08-22 15:33:02 -0600

    Clases principales

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-class Prodcutos:
-    def __init__(self, IdProducto, Nombre,Precio,TotalCompras,TotalVentas,Stock,IdCategoria):
+class Productos:
+    def __init__(self, IdProducto, Nombre, Precio, TotalCompras, TotalVentas, Stock, IdCategoria):
         self.IdProducto = IdProducto
         self.Nombre = Nombre
         self.Precio = Precio
@@ -7,28 +7,35 @@ class Prodcutos:
         self.TotalVentas = TotalVentas
         self.Stock = Stock
         self.IdCategoria = IdCategoria
+
+
 class Categorias:
     def __init__(self, IdCategoria, Nombre):
         self.IdCategoria = IdCategoria
         self.Nombre = Nombre
+
+
 class Clientes:
-    def __init__(self,NitCliente,Nombre,Direccion,Telefono,Correo):
+    def __init__(self, NitCliente, Nombre, Direccion, Telefono, Correo):
         self.NitCliente = NitCliente
         self.Nombre = Nombre
         self.Direccion = Direccion
         self.Telefono = Telefono
         self.Correo = Correo
 
+
 class Proveedores:
-    def __init__(self,NitProveedor,Nombre,Direccion,Telefono,Correo,Empresa):
+    def __init__(self, NitProveedor, Nombre, Direccion, Telefono, Correo, Empresa):
         self.NitProveedor = NitProveedor
         self.Nombre = Nombre
         self.Direccion = Direccion
         self.Telefono = Telefono
         self.Correo = Correo
         self.Empresa = Empresa
+
+
 class Empleados:
-    def __init__(self,IdEmpleado,Nombre,Direccion,Telefono,Correo,IdPuesto):
+    def __init__(self, IdEmpleado, Nombre, Direccion, Telefono, Correo, IdPuesto):
         self.IdEmpleado = IdEmpleado
         self.Nombre = Nombre
         self.Direccion = Direccion
@@ -36,15 +43,18 @@ class Empleados:
         self.Correo = Correo
         self.IdPuesto = IdPuesto
 
+
 class Ventas:
-    def __init__(self, IdVentas,Fecha,IdEmpleado,NitCliente, Total):
+    def __init__(self, IdVentas, Fecha, IdEmpleado, NitCliente, Total):
         self.IdVentas = IdVentas
         self.Fecha = Fecha
         self.IdEmpleado = IdEmpleado
         self.NitCliente = NitCliente
         self.Total = Total
+
+
 class DetallesVentas:
-    def __init__(self, IdDetallesVentas, IdVentas, IdProducto,Cantidad,SubTotal,Stock):
+    def __init__(self, IdDetallesVentas, IdVentas, IdProducto, Cantidad, SubTotal, Stock):
         self.IdDetallesVentas = IdDetallesVentas
         self.IdVentas = IdVentas
         self.IdProducto = IdProducto
@@ -52,25 +62,26 @@ class DetallesVentas:
         self.SubTotal = SubTotal
         self.Stock = Stock
 
+
 class Compras:
-    def __init__(self, IdCompra, FechaIngreso,IdEmpleado, NitProveedor, Total):
+    def __init__(self, IdCompra, FechaIngreso, IdEmpleado, NitProveedor, Total):
         self.IdCompra = IdCompra
         self.FechaIngreso = FechaIngreso
         self.IdEmpleado = IdEmpleado
         self.NitProveedor = NitProveedor
         self.Total = Total
 
+
 class DetallesCompras:
-    def __init__(self,IdDetalleCompra, IdCompra, Cantidad, IdProducto, Subtotal):
+    def __init__(self, IdDetalleCompra, IdCompra, Cantidad, IdProducto, SubTotal):
         self.IdDetalleCompra = IdDetalleCompra
         self.IdCompra = IdCompra
         self.Cantidad = Cantidad
         self.IdProducto = IdProducto
-        self.SubTotal = Subtotal
+        self.SubTotal = SubTotal
 
 
 class Puesto:
     def __init__(self, IdPuesto, Nombre):
         self.IdPuesto = IdPuesto
         self.Nombre = Nombre
-
