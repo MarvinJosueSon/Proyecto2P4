aabb38b1568274105859b7952670ae7b79664f90
Marvin <sonmarvint2001@gmail.com>
2025-08-22 14:39:58 -0600

    Primer Clases

---
+++ main.py
@@ -0,0 +1,9 @@
+class Prodcutos:
+    def __init__(self, IdProducto, Nombre,Precio,TotalCompras,TotalVentas,Stock,IdCategoria):
+        self.IdProducto = IdProducto
+        self.Nombre = Nombre
+        self.Precio = Precio
+        self.TotalCompras = TotalCompras
+        self.TotalVentas = TotalVentas
+        self.Stock = Stock
+        self.IdCategoria = IdCategoria
