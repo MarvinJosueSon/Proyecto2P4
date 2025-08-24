3d2db81ab87f4e67eee59a04849ba41707eb69e7
Marvin <sonmarvint2001@gmail.com>
2025-08-24 00:21:02 -0600

    Avance

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+productosDiccionario = {}
+clientesDiccionario = {}
+categoriasDiccionario={}
+proveedoresDiccionario={}
+empleadosDiccionario={}
+ventasDiccionario={}
+detallesVentasDiccionario={}
+comprasDiccionario={}
+detallesComprasDiccionario={}
+
 class Productos:
     def __init__(self, IdProducto, Nombre, Precio, TotalCompras, TotalVentas, Stock, IdCategoria):
         self.IdProducto = IdProducto
@@ -85,3 +95,83 @@ class Puesto:
     def __init__(self, IdPuesto, Nombre):
         self.IdPuesto = IdPuesto
         self.Nombre = Nombre
+
+"""
+INICIO DE CLASES SECUNDARIAS
+"""
+class Validar:
+    def validarCodigo(self,diccionario):
+        try:
+            while True:
+                codigoAux = input("Ingrese el codigo unico: ")
+                if not codigoAux in diccionario:
+                    return codigoAux
+                else:
+                    print("El codigo no debe repetirse")
+        except ValueError:
+            print("El codigo no es valido")
+
+    def validarNombre(self):
+        try:
+            while True:
+                nombreAux = input("Ingrese el nombre: ")
+                if nombreAux != "":
+                    return nombreAux
+                else:
+                    print("El nombre no puede estar vacio")
+        except ValueError:
+            print("El nombre no es valido")
+    def validarDireccion(self):
+        try:
+            while True:
+                direccionAux = input("Ingrese la direccion: ")
+                if direccionAux != "":
+                    return direccionAux
+                else:
+                    print("La direccion no puede estar vacia")
+        except ValueError:
+            print("la direccion no es valida")
+
+    def validarTelefono(self):
+        try:
+            while True:
+                telefonoAux = int(input("Ingrese el telefono del producto: "))
+                if telefonoAux>=0:
+                    return telefonoAux
+                else:
+                    print("El telefono no es valido")
+        except ValueError:
+            print("El telefono no es valido")
+    def validarCorreo(self):
+        try:
+            while True:
+                correoAux = input("Ingrese el correo del producto: ")
+                if ("@" in correoAux) and ("." in correoAux):
+                    return correoAux
+                else:
+                    print("El correo no es valido")
+        except ValueError:
+            print("El correo no es valido")
+
+class administrarCategoria:
+
+    def agregarCategoria(self,diccionario):
+        validar = Validar()
+        codigo=validar.validarCodigo(categoriasDiccionario)
+        nombre=validar.validarNombre()
+        categoriaAux= Categorias(codigo,nombre)
+        categoriasDiccionario[codigo]= {
+            "categoria":categoriaAux
+        }
+
+    def eliminarCategoria(self, diccionario):
+        if not diccionario:
+            print("No hay categorías para eliminar")
+            return
+
+        codigo = input("Ingrese el código de la categoría a eliminar: ")
+        if codigo in diccionario:
+            del diccionario[codigo]
+            print(f"Categoría {codigo} eliminada")
+        else:
+            print("La categoría no existe")
